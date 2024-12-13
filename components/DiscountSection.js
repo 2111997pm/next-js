d28b545080ffedbe033272e4cec2a1441b5e00dc
Prahalad <prahalad@Prahalads-MacBook-Air.local>
2024-12-13 17:46:16 +0530

    remove min height

--- components/DiscountSection.js
+++ components/DiscountSection.js
@@ -2,8 +2,7 @@ import React from "react";
 
 export default function DiscountSection() {
   return (
-    <div className="flex flex-col lg:flex-row justify-between p-6 bg-gray-50 min-h-screen mt-5 space-y-6 lg:space-y-0 lg:space-x-6">
-      {/* Discount Codes Section */}
+    <div className="flex flex-col lg:flex-row justify-between p-6 bg-gray-50  mt-5 space-y-6 lg:space-y-0 lg:space-x-6">
       <div className="w-full lg:w-2/3 bg-white p-6 rounded-lg shadow-md">
         <h2 className="text-xl font-semibold text-gray-800 mb-4">
           Discount Codes
@@ -26,7 +25,6 @@ export default function DiscountSection() {
         </button>
       </div>
 
-      {/* Summary Section */}
       <div className="w-full lg:w-1/3 bg-teal-50 p-6 rounded-lg shadow-md">
         <h2 className="text-xl font-semibold text-gray-800 mb-6">
           Order Summary
